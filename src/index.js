2e39a940e0b1f9dc2931a46f272550513bf77ce6
oleg291291@mail.ru <oleg291291@mail.ru>
2018-02-28 18:32:21 +0300

    about 73pass

--- src/index.js
+++ src/index.js
@@ -3,80 +3,41 @@ module.exports = function longestConsecutiveLength(array) {
 
 
 
-var num = 0;
-var maxCounter = 0;
-var step = 0;
-//var cache = "";
+  var num = 0;
+  var maxCounter = 0;
+  var step = 0;
 
-function loop(){
-  
-  if(array.indexOf(num) != -1 && cache.indexOf("_" + num + "_") == -1){
-    
-    cache = cache + "_" + num + "_";
-  num++;
-  counter++;
-  array[step].delete;
-  loop();
+
+  function loop() {
+    var location = array.indexOf(num);
+
+    if (location != -1) {
+      //cache = cache + "_" + num + "_";
+      num++;
+      counter++;
+      delete array[location];
+      loop();
+    }
+  }
+
+  while (step < array.length) {
+    var counter = 0;
+    num = array[step];
+    if (num != undefined) {
+      loop();
+      if (counter > maxCounter) {
+        maxCounter = counter;
+      }
+    }
+
+
+
+    step++;
+  }
+
+
+  return maxCounter;
+
 }
-}
-
-while(step < array.length){
- var counter = 0;
-num = array[step];
-loop();
-if(counter > maxCounter){
-  maxCounter = counter;
-}
-
-step++;
-}
 
 
-return maxCounter;
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-//   if (array.length == 0) {
-//     return 0;
-//   }
-// //последовательность не может быть длинее длинны массива.
-//   var num = 0;
-//   var counter = 0;
-//   var maxCounter = 0;
-//   do {
-//     if (array.indexOf(num) == -1) {
-//       num++;
-//     }
-//   } while (array.indexOf(num) == -1);
-
-// function seqSearch(num){
-//   for (var i = 0; i < array.length; i++) {
-//     if(array[i] == num){
-//       num++;
-//       counter++;
-//       if(maxCounter < counter){
-//         maxCounter = counter;
-//       }
-//       seqSearch(num);
-//     }
-//   } 
-// }
-
-// for (var step = 0; step < array.length; step++){
-//   seqSearch(num);
-// }
-
-
-// return maxCounter;
-// }
